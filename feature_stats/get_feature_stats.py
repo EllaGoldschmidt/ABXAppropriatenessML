d1fbdd30e0166e6de752b3e162b9c2e5d939fd8d
EllaRan <ellarannon@gmail.com>
2023-07-11 11:56:23 +0300

    Create get_feature_stats.py

---
+++ feature_stats/get_feature_stats.py
@@ -0,0 +1,38 @@
+from scipy.stats import ttest_ind
+import pandas as pd
+from sklearn.feature_selection import chi2
+from statsmodels.stats.multitest import fdrcorrection
+import numpy as np
+from math import log10
+
+
+def apply_FDR_correction(df, column, alpha):
+    relevant_values = df[pd.notnull(df[column])][column]
+    _, df.loc[relevant_values.index, column] = fdrcorrection(relevant_values, alpha)
+    df[column+"_exponent"] = df[column].apply(lambda x: np.nan if pd.isnull(x) else round(-log10(x), 2))
+    df[column] = np.round(df[column], decimals=2)
+
+
+def get_cont_features_stats(df, cont_features, file_prefix):
+    stats = pd.DataFrame(cont_features, columns=["feature"])
+    positive, negative = df[df["target"] == 1][cont_features], df[df["target"] == 0][cont_features]
+    _, stats["t-test p-value"] = ttest_ind(positive, negative, equal_var=False, nan_policy='omit')
+    apply_FDR_correction(stats, "t-test p-value", 0.025)
+    stats.round(2).to_csv(file_prefix + "_cont_stats.csv", index=False)
+    return list(stats[stats['t-test p-value'] < 0.05]['feature'])
+
+
+def get_categorical_features_stats(df, cat_features, file_prefix):
+    """"""
+    rel_df, y = df[cat_features], df['target']
+    # getting only columns with all positive values, as chi test only works with positive values
+    positive_df = rel_df.loc[:, rel_df.ge(0).all()]
+    positive_columns = list(positive_df.columns)
+    chi2_p_values = chi2(positive_df, y)[1]
+
+    result_dict = {feat: {"chi2 p-value": np.nan} for feat in cat_features}
+    result_dict.update({positive_columns[i]: {"chi2 p-value": chi2_p_values[i]} for i in range(len(positive_columns))})
+    stats = pd.DataFrame.from_dict(result_dict, orient='index').reset_index().rename(columns={"index": "feature"})
+    apply_FDR_correction(stats, "chi2 p-value", 0.05)
+    stats.round(2).to_csv(file_prefix + "_categorical_stats.csv", index=False)
+    return list(stats[stats["chi2 p-value"] < 0.05]['feature'])
